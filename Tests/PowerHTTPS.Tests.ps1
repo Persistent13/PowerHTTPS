d8bf745b6a04d74e2a599a088f3d2e3129885a85
Dakota Clark <dakota@allclarks.com>
2016-11-20 21:21:51 -0800

    Updated test to work.

--- Tests/PowerHTTPS.Tests.ps1
+++ Tests/PowerHTTPS.Tests.ps1
@@ -1,15 +1,17 @@
-﻿Import-Module $PSScriptRoot\..\PowerHTTPS\PowerHTTPS.psd1 -Force
+﻿$WorkspaceRoot = $(Get-Item $PSScriptRoot).Parent.FullName
+Remove-Module PowerHTTPS -ErrorAction Ignore
+Import-Module $WorkspaceRoot\PowerHTTPS\PowerHTTPS.psd1 -Force
 
 InModuleScope PowerHTTPS {
     Describe PowerHTTPS {
         It 'Retrieves engine and assessment metadata from SSL Labs' {
             $info = Get-SLInfo
             $info.EngineVersion | Should BeOfType [version]
-            $info.RatingCriteriaVersion | Should Exist
-            $info.MaxAssessments | Should Exist
-            $info.ActiveAssessments | Should Exist
-            $info.NewAssessmentCoolOff | Should Exist
-            $info.Message | Should Exist
+            $info.RatingCriteriaVersion | Should Not BeNullOrEmpty
+            $info.MaxAssessments | Should Not BeNullOrEmpty
+            $info.ActiveAssessments | Should Not BeNullOrEmpty
+            $info.NewAssessmentCoolOff | Should Not BeNullOrEmpty
+            $info.Message | Should Not BeNullOrEmpty
         }
         It 'Starts an HTTPS security analysis on a given site' {
             $pub = Start-SLEndpointAnalysis -Name www.microsoft.com -Public
@@ -18,19 +20,19 @@ InModuleScope PowerHTTPS {
             $pub.StartTime | Should BeOfType [datetime]
             $pub.StartTime | Should Not Be [datetime]'1/1/1970'
             $pub.EngineVersion | Should BeOfType [version]
-            $pub.RatingCriteriaVersion | Should Exist
-            $pub.Endpoints | Should Be $null
+            $pub.RatingCriteriaVersion | Should Not BeNullOrEmpty
         }
         It 'Retrieves the data for a previously run analysis' {
             # Loop until the assessment completes.
-            # After the 12th try the script will exit.
+            # After the 31st (10 min) try the script will exit.
             $i=0
-            do {
+            $data = Get-SLEndpointAnalysis -Name www.microsoft.com -DisableCache
+            while ( $data.Status -ne 'READY' -and $data.Status -ne 'ERROR' ) {
                 Start-Sleep -Seconds 10
                 $data = Get-SLEndpointAnalysis -Name www.microsoft.com -DisableCache
                 $i++
-                if($i -ge 13){ throw 'Unable to retrieve endpoint analysis with Get-SLEndpointAnalysis.' }
-            } while ( $data.Status -ne 'READY' -or $data.Status -ne 'ERROR' )
+                if($i -ge 62){ continue }
+            }
 
             $data.Host | Should BeExactly 'www.microsoft.com'
             $data.Public | Should Be $true
@@ -39,7 +41,7 @@ InModuleScope PowerHTTPS {
             $data.AnalysisTime | Should BeOfType [datetime]
             $data.AnalysisTime | Should Not Be [datetime]'1/1/1970'
             $data.EngineVersion | Should BeOfType [version]
-            $data.RatingCriteriaVersion | Should Exist
+            $data.RatingCriteriaVersion | Should Not BeNullOrEmpty
             $data.Endpoints | Should Not Be $null
             foreach($ep in $data.Endpoints){
                 $ep.Details | Should Not Be $null
